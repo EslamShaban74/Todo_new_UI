a936a519394e428690d5cd33b2969c038dff6dfb
Eslam <eslamshaban12328@gmail.com>
2022-01-16 20:50:16 +0200

    scheduled notification

--- lib/db/db_helper.dart
+++ lib/db/db_helper.dart
@@ -1,1 +1,33 @@
-class DBHelper {}
+import 'package:flutter/material.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:todo/models/task.dart';
+
+class DBHelper {
+  static Database? _db;
+  static const int _version = 1;
+  static const String _tableName = 'tasks';
+
+  static Future<void> initDb() async {
+    if (_db != null) {
+      debugPrint('not null');
+    } else {
+      try {
+        String _path = await getDatabasesPath() + 'task.db';
+        debugPrint('in database path');
+        _db = await openDatabase(_path, version: _version,
+            onCreate: (Database db, int version) async {
+          debugPrint('create a new one');
+          await db.execute('CREATE TABLE $_tableName ('
+              'id INTEGER PRIMARY KEY AUTOINCREMENT, '
+              'title STRING, note TEXT, date STRING, '
+              'startTime STRING, endTime STRING, '
+              'remind INTEGER, repeat STRING, '
+              'color INTEGER, '
+              'isCompleted INTEGER)');
+        });
+      } catch (e) {
+        debugPrint("$e");
+      }
+    }
+  }
+}
